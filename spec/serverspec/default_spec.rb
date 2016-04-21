310b5b321fc0a66a5ba2c8399063edefeae74df4
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-04-21 09:36:29 +0900

    start resourcemanager
    also, remove useless tests like match /<value>100</value>/. xmlpath test
    is needed

--- spec/serverspec/default_spec.rb
+++ spec/serverspec/default_spec.rb
@@ -5,22 +5,32 @@ package = 'hadoop'
 service = 'namenode'
 user    = 'hdfs'
 group   = 'hadoop'
-ports   = [ 8020, 50070 ]
+resourcemanager_service = 'resourcemanager'
+
+# tcp4  0/0/128        127.0.0.1.8020
+# tcp4  0/0/128        *.50070
+# tcp4  0/0/128        127.0.0.1.8033
+# tcp4  0/0/128        127.0.0.1.8088
+# tcp4  0/0/128        127.0.0.1.8032
+# tcp4  0/0/128        127.0.0.1.8030
+# tcp4  0/0/128        127.0.0.1.8031
+
+ports   = [ 8020, 50070, 8033, 8088, 8032, 8030, 8031 ]
 log_dir = '/var/log/hadoop'
 db_dir  = '/var/lib/hadoop'
-
-core_site_xml = '/etc/hadoop/core-site.xml'
-hdfs_site_xml = '/etc/hadoop/hdfs-site.xml'
-mapred_site_xml = '/etc/hadoop/mapred-site.xml'
+conf_dir = '/etc/hadoop'
 
 case os[:family]
 when 'freebsd'
   package = 'hadoop2'
   db_dir = '/var/db/hadoop'
-  core_site_xml = '/usr/local/etc/hadoop/core-site.xml'
-  hdfs_site_xml = '/usr/local/etc/hadoop/hdfs-site.xml'
-  mapred_site_xml = '/usr/local/etc/hadoop/mapred-site.xml'
+  conf_dir = '/usr/local/etc/hadoop'
 end
+
+core_site_xml   = "#{conf_dir}/core-site.xml"
+hdfs_site_xml   = "#{conf_dir}/hdfs-site.xml"
+yarn_site_xml   = "#{conf_dir}/yarn-site.xml"
+mapred_site_xml = "#{conf_dir}/mapred-site.xml"
 
 describe package(package) do
   it { should be_installed }
@@ -45,7 +55,6 @@ describe file(hdfs_site_xml) do
   its(:content) { should match Regexp.escape('<name>dfs.blocksize</name>') }
   its(:content) { should match Regexp.escape('<value>268435456</value>') }
   its(:content) { should match Regexp.escape('<name>dfs.namenode.handler.count</name>') }
-  its(:content) { should match Regexp.escape('<value>100</value>') }
   its(:content) { should match Regexp.escape('<name>dfs.datanode.data.dir</name>') }
   its(:content) { should match Regexp.escape("<value>${hadoop.tmp.dir}/dfs/data</value>") }
 end
@@ -55,6 +64,22 @@ describe file(mapred_site_xml) do
   its(:content) { should match Regexp.escape('<?xml-stylesheet type="text/xsl" href="configuration.xsl"?>') }
   its(:content) { should match Regexp.escape('<name>mapreduce.framework.name</name>') }
   its(:content) { should match Regexp.escape('<value>-Xmx512M</value>') }
+end
+
+describe file(yarn_site_xml) do
+  it { should be_file }
+  its(:content) { should match Regexp.escape('<?xml-stylesheet type="text/xsl" href="configuration.xsl"?>') }
+  its(:content) { should match Regexp.escape('<name>yarn.resourcemanager.scheduler.class</name>') }
+  its(:content) { should match Regexp.escape('<value>org.apache.hadoop.yarn.server.resourcemanager.scheduler.capacity.CapacityScheduler</value>') }
+  its(:content) { should match Regexp.escape('<name>yarn.acl.enable</name>') }
+  its(:content) { should match Regexp.escape('<name>yarn.admin.acl</name>') }
+  its(:content) { should match Regexp.escape('<name>yarn.log-aggregation-enable</name>') }
+  its(:content) { should match Regexp.escape('<name>yarn.resourcemanager.hostname</name>') }
+  its(:content) { should match Regexp.escape('<name>yarn.scheduler.capacity.root.queues</name>') }
+  its(:content) { should match Regexp.escape('<name>yarn.scheduler.capacity.root.default.capacity</name>') }
+  its(:content) { should match Regexp.escape('<name>yarn.scheduler.capacity.root.default.state</name>') }
+  its(:content) { should match Regexp.escape('<name>yarn.scheduler.capacity.root.default.user-limit-factor</name>') }
+  its(:content) { should match Regexp.escape('<name>yarn.scheduler.capacity.root.default.maximum-capacity</name>') }
 end
 
 describe file(log_dir) do
@@ -79,6 +104,11 @@ end
 # end
 
 describe service(service) do
+  it { should be_running }
+  it { should be_enabled }
+end
+
+describe service(resourcemanager_service) do
   it { should be_running }
   it { should be_enabled }
 end
