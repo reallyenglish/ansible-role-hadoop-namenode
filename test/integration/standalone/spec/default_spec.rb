12b14a5383dd60f9b912944dbf0203b2ceaf2ab6
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-06-22 15:15:49 +0900

    use retry_and_sleep to remove a race
    because "State" can be 'initializing', which means the service is not
    ready.

--- test/integration/standalone/spec/default_spec.rb
+++ test/integration/standalone/spec/default_spec.rb
@@ -3,17 +3,6 @@ require 'json'
 
 class ServiceNotReady < StandardError
 end
-# {
-#   "beans" : [ {
-#     "name" : "Hadoop:service=NameNode,name=NameNodeStatus",
-#     "modelerType" : "org.apache.hadoop.hdfs.server.namenode.NameNode",
-#     "State" : "active",
-#     "NNRole" : "NameNode",
-#     "HostAndPort" : "localhost:8020",
-#     "SecurityEnabled" : false,
-#     "LastHATransitionTime" : 0
-#   } ]
-# }
 describe server(:namenode) do
 
   it 'should be ready' do
@@ -25,10 +14,24 @@ describe server(:namenode) do
   end
 
   it 'should return NameNodeStatus == active' do
-    http_res = fetch "http://#{ server(:namenode).server.address }:50070/jmx?qry=Hadoop:service=NameNode,name=NameNodeStatus"
-    json = JSON.parse(http_res.body)
-    expect(json.class).to eq(Hash)
-    res = json['beans'].first
+    # {
+    #   "beans" : [ {
+    #     "name" : "Hadoop:service=NameNode,name=NameNodeStatus",
+    #     "modelerType" : "org.apache.hadoop.hdfs.server.namenode.NameNode",
+    #     "State" : "active",
+    #     "NNRole" : "NameNode",
+    #     "HostAndPort" : "localhost:8020",
+    #     "SecurityEnabled" : false,
+    #     "LastHATransitionTime" : 0
+    #   } ]
+    # }
+    res = nil
+    retry_and_sleep do
+      http_res = fetch "http://#{ server(:namenode).server.address }:50070/jmx?qry=Hadoop:service=NameNode,name=NameNodeStatus"
+      json = JSON.parse(http_res.body)
+      res = json['beans'].first
+      raise ServiceNotReady if res['State'] == 'initializing'
+    end
     expect(res['name']).to eq('Hadoop:service=NameNode,name=NameNodeStatus')
     expect(res['NNRole']).to eq('NameNode')
     expect(res['State']).to eq('active')
